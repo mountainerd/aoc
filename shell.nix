5c2fcd25c9958f7a0ca9da9cf86c29a43316ca9c
Joshua Blair <jwblair@gmail.com>
2022-12-03 14:35:23 -0700

    Setting up environment for future me.

---
+++ shell.nix
@@ -0,0 +1,12 @@
+{ pkgs ? import <nixpkgs> {} }:
+   pkgs.mkShell {
+      # Environment variables
+      GIT_AUTHOR_EMAIL = "jwblair@gmail.com";
+      GIT_AUTHOR_NAME = "Joshua Blair";
+      GIT_COMMITTER_NAME = "Joshua Blair";
+      GIT_COMMITTER_EMAIL = "jwblair@gmail.com";
+      # Development
+      nativeBuildInputs = with pkgs; [
+         go
+      ];
+   }
